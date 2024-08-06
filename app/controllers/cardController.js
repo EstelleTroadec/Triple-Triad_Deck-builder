cfa21817fd95ae463d1ac4b9c59c585e7ea8b42d
Estelle Troadec <estelle.t29@gmail.com>
2024-08-06 16:13:02 +0200

    feat: create a card page view, along with its controller & route

---
+++ app/controllers/cardController.js
@@ -0,0 +1,29 @@
+const dataMapper = require('../dataMapper.js');
+
+const cardController = {
+
+    cardPage: async (req, res) => {
+
+        // get the id from the url
+        // parseInt = converts a string to an integer
+        // 10 = makes sure it's in a decimal base
+        const cardId = parseInt(req.params.id, 10);
+
+        try {
+            const card = await dataMapper.getCard(cardId);
+
+            if (card) {
+                // if card found, render the card page
+                res.render('cardPage', {card});
+            } else {
+                // if card not found, return a 404 error
+                res.status(404).send(`Card with the id ${cardId} not found`);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(`An error occured with the database :${error}`);
+        }
+    }
+}
+
+module.exports = cardController;
--- app/dataMapper.js
+++ app/dataMapper.js
@@ -1,10 +1,26 @@
-const database = require('./database');
+const client = require('./database');
 
 const dataMapper = {
   async getAllCards() {
-    const query = "SELECT * FROM card";
-    const result = await database.query(query);
+    const query = 'SELECT * FROM card';
+    const result = await client.query(query);
     return result.rows;
+  },
+
+
+  async getCard(cardId) {
+    const query = {
+      // select all the fields of the card with the given id
+      text: `SELECT * FROM card WHERE id = $1`,
+      // array with the values to be used in the query (instead of $1)
+      values: [cardId],
+    }
+
+    // execute the previous query, using the database
+    const result = await client.query(query);
+
+    //return the result
+    return result.rows[0];
   }
 };
 
